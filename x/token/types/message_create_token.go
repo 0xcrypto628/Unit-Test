a1fd6c9dea629f8b871e275c62ebb8e499570ceb
0xcrypto628 <minatosakai0628@gmail.com>
2024-09-14 17:21:03 +0900

    Validation for MsgCreateToken

--- x/token/types/message_create_token.go
+++ x/token/types/message_create_token.go
@@ -1,28 +1,42 @@
 package types
 
 import (
-	errorsmod "cosmossdk.io/errors"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+    "strconv" // for converting strings to integers
+    errorsmod "cosmossdk.io/errors"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 var _ sdk.Msg = &MsgCreateToken{}
 
-
-
+// NewMsgCreateToken creates a new instance of MsgCreateToken with the provided arguments
 func NewMsgCreateToken(creator string, name string, symbol string, totalSupply string) *MsgCreateToken {
-	return &MsgCreateToken{
-		Creator:     creator,
-		Name:        name,
-		Symbol:      symbol,
-		TotalSupply: totalSupply,
-	}
+    return &MsgCreateToken{
+        Creator:     creator,
+        Name:        name,
+        Symbol:      symbol,
+        TotalSupply: totalSupply,
+    }
 }
 
+// ValidateBasic ensures that the fields of the message are valid
 func (msg *MsgCreateToken) ValidateBasic() error {
-	_, err := sdk.AccAddressFromBech32(msg.Creator)
-	if err != nil {
-		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
-	}
-	return nil
+    if len(msg.Name) == 0 {
+        return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "token name cannot be empty")
+    }
+    if len(msg.Symbol) == 0 {
+        return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "token symbol cannot be empty")
+    }
+
+    // Convert TotalSupply from string to uint64
+    totalSupply, err := strconv.ParseUint(msg.TotalSupply, 10, 64)
+    if err != nil {
+        return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "total supply must be a valid unsigned integer")
+    }
+    
+    if totalSupply <= 0 {
+        return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "total supply must be greater than zero")
+    }
+
+    return nil
 }
